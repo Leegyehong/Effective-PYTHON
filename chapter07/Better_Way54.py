aa8e8a5840f1807ff8d07eb48c7428570f41ee2f
Leegyehong <59252624+Leegyehong@users.noreply.github.com>
2022-06-06 02:51:03 +0900

    add: chapter07 _ 54

--- chapter07/Better_Way54.py
+++ chapter07/Better_Way54.py
@@ -51,3 +51,45 @@ print(f'카운터 값은 {expected}여야 하는데, 실제로는 {found} 입니
 # 이를 위해 실행중인 스레드를 일시 중단 시키고 다른 스레드를 실행시키는 일을 반복한다
 # 문제는 스레드를 언제 일시 중단 시킬지 알 수 없다는 점
 
+
+# 데이터 경합이나 다른 유형의 데이터 구조 오염을 해결하기 위해
+# 파이썬은 threading 내장 모듈 안에 여러가지 도구를 제공한다
+# 가장 간단하지만 유용한 도구로 Lock 클래스(뮤텍스) 가 있다
+from threading import Thread
+import select
+import socket
+
+from threading import Lock
+
+class LockingCounter:
+    def __init__(self):
+        self.lock = Lock()
+        self.count = 0
+
+    def increment(self, offset):
+        with self.lock:
+            self.count += offset
+
+def worker(sensor_index, how_many, counter):
+    for _ in range(how_many):
+        # 센서를 읽는다
+        counter.increment(1)
+
+from threading import Thread
+
+how_many = 10**5
+counter = LockingCounter()
+
+threads = []
+for i in range(5):
+    thread = Thread(target=worker,
+                    args=(i, how_many, counter))
+    threads.append(thread)
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+expected = how_many * 5
+found = counter.count
+print(f'카운터 값은 {expected}여야 하는데, 실제로는 {found} 입니다')
