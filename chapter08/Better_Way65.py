bd0778eca7af641c9c96a2b9b419b3a5ea1a06e1
Leegyehong <59252624+Leegyehong@users.noreply.github.com>
2022-06-14 00:02:54 +0900

    fix: chapter07 _ 65

--- chapter08/Better_Way65.py
+++ chapter08/Better_Way65.py
@@ -1,6 +1,11 @@
-#
-# 아이템 65
-#
+# try/except/else/finally의 각 블록을 잘 활용하라
+
+# 파이썬에서 예외를 처리하는 과정에서는 특정 동작을 수행하고 싶은 네 가지 경우가 있다
+# try, except, else, finally 
+# 각각은 서로 다른 목적에 쓰이며, 다양하게 조합하면 유용하다
+
+# finally 
+# 예외가 발생하더라도 정리코드를 실행해야 한다면 try/finally를 사용하라
 
 def try_finally_example(filename):
     print('* 파일 열기')
@@ -24,7 +29,13 @@ with open(filename, 'wb') as f:
 # 오류가 나는 부분. 오류를 보고 싶으면 커멘트를 해제할것
 #try_finally_example('does_not_exist.txt')
 
-#
+
+# else
+# 코드에서 처리할 예외와 호출 스택을 거슬러 올라가며 전달할 예외를 명확히 구분하기 위해
+# try/catch/else를 사용하라
+# try에서 예외가 발생하지 않으면 elser가 실행됨
+# else를 사용하면 try 안에 들어갈 코드를 최소화 한다
+# try에 들어가는 코드가 줄어들면 발생할 여지가 있는 예외를 서로 구분할 수 있으므로 가독성이 좋아진다
 import json
 
 def load_json_key(data, key):
@@ -46,7 +57,10 @@ assert load_json_key('{"foo": "bar"}', 'foo') == 'bar'
 # 오류가 나는 부분. 오류를 보고 싶으면 커멘트를 해제할것
 #load_json_key('{"foo": "bar"}', '존재하지 않음')
 
-#
+
+# 모든 요소를 한꺼번에 사용하기
+# 복합적인 문장 안에 모든 요소를 다 넣고 싶다면
+# try/except/else/finally 를 사용하라
 UNDEFINED = object()
 
 def divide_json(path):
