34ab73b2b95f80af8a8d3dd937d34f4d45f3a2a2
Leegyehong <59252624+Leegyehong@users.noreply.github.com>
2022-06-18 23:24:14 +0900

    fix: chapter08 _ 68

--- chapter08/Better_Way68.py
+++ chapter08/Better_Way68.py
@@ -1,6 +1,10 @@
-#
-# 아이템 68
-#
+# copyreg를 사용해 pickle을 더 신뢰성 있게 만들라
+
+# pickle 내장 모듈을 사용하면 파이썬 객체를 바이트 스트림으로 직렬화 하거나
+# 바이트 스트림을 파이썬 객체로 역직렬화할 수 있다
+
+# 바이트 스트림은 서로 신뢰할 수 없는 당사자 사이의 통신에 사용하면 안된다
+# pickle의 목적은 제어하는 프로그램들이 이진 채널을 통해 서로 파이썬 객체를 넘기는 데 있다.
 class GameState:
     def __init__(self):
         self.level = 0
@@ -42,7 +46,10 @@ print(state_after.__dict__)
 
 assert isinstance(state_after, GameState)
 
-#
+# 위 방식 처럼 pickle을 사용하는 방식이 아주 간단한 수준을 벗어나는 순간, pickle 모듈의 동작은 예상할 수 없는 방식으로 망가지기 시작한다.
+
+# 디폴트 애트리뷰트 값
+# 가장 간단한 경우, 디폴트 인자가 있는 생성자를 사용하면 언피클 했을 때도 항상 필요한 모든 애트리뷰트를 포함시킬 수 있다.
 class GameState:
     def __init__(self, level=0, lives=4, points=0):
         self.level = level
@@ -77,7 +84,8 @@ print('이전:', state.__dict__)
 state_after = pickle.loads(serialized)
 print('이후:', state_after.__dict__)
 
-#
+# 클래스 버전 지정
+# 파이썬 객체의 필드를 제거해 예전 버전 객체와의 하위 호환성이 없어지는 경우도 발생한다
 class GameState:
     def __init__(self, level=0, points=0, magic=5):
         self.level = level
@@ -103,7 +111,9 @@ print('이전:', state.__dict__)
 state_after = pickle.loads(serialized)
 print('이후:', state_after.__dict__)
 
-#
+# 안정적인 임포트 경로
+# pickle을 할 때 마주칠수 있는 다른 문제점으로
+# 클래스 이름이 바뀌어 코드가 깨지는 경우를 들 수 있다
 class BetterGameState:
     def __init__(self, level=0, points=0, magic=5):
         self.level = level
