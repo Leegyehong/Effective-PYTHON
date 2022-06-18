b041893555477af939d40d94dfe5c5a3530ac9df
Leegyehong <59252624+Leegyehong@users.noreply.github.com>
2022-06-18 14:45:28 +0900

    fix: chapter08 _ 66

--- chapter08/Better_Way66.py
+++ chapter08/Better_Way66.py
@@ -1,11 +1,17 @@
 # 재사용 가능한 try/finally 동작을 원한다면 contextlib과 with 문을 사용하라
+
+# with문은 코드가 특별한 컨텍스트 안에서 실행되는 경우를 표현한다
+# ex) 뮤텍스를 with문 안에서 사용하면 락을 소유했을 때만 코드가 실행된다는 것을 의미
 import logging
 
 def my_function():
     logging.debug('디버깅 데이터')
     logging.error('이 부분은 오류 로그')
     logging.debug('추가 디버깅 데이터')
+# 프로그램의 디폴트 로그 수준은 WARNING이다
+# 위 함수를 실행하면 error 메시지만 출력됨
 
+# 컨텍스트 매니저를 정의하면 이 함수의 로그 수준을 일시적으로 높일 수 있다
 from contextlib import contextmanager
 
 @contextmanager
@@ -25,10 +31,11 @@ with debug_logging(logging.DEBUG):
 print('* 외부:')
 my_function()
 
-#
+# with와 대상 변수 함께 사용하기
 with open('my_output.txt', 'w') as handle:
     handle.write('데이터입니다!')
 
+# as 대상 변수에게 값을 제동하도록 하기 위해 필요한 일은 컨텍스트 매니저 안에서 yield 값을 넘기는 것뿐이다. 
 @contextmanager
 def log_level(level, name):
     logger = logging.getLogger(name)
